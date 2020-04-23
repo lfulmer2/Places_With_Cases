f12dad5ec5df34192a96450b9f59631c0296b74c
nikhilkhandekar <nikhilk5@illinois.edu>
2020-04-22 23:10:39 -0400

    Hi!

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -11,4 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //We'll start here tomorrow!
+
 }
