4150b7e12509a48c3b49f94d5919f1faf8caf5c8
Leo <lfulmer2@illinois.edu>
2020-04-29 05:48:56 -0400

    Finished the UI mockup, pressing buttons store variables within their host activity, we need to have the variables carry over to the output Activity so that we can use them in the logic calculations.

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -2,17 +2,54 @@ package com.example.cs125finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button fifteenMiles;
+    private Button thirtyMiles;
+    private Button fiftyMiles;
+    public int mileRadius;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        private String test = "this is a test variable";
-        System.out.println(test);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fifteenMiles = (Button) findViewById(R.id.fifteenMiles);
+        thirtyMiles = (Button) findViewById(R.id.thirtyMiles);
+        fiftyMiles = (Button) findViewById(R.id.fiftyMiles);
+
+
+        fifteenMiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStorePicker();
+                mileRadius = 15;
+            }
+        });
+        thirtyMiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStorePicker();
+                mileRadius = 30;
+            }
+        });
+        fiftyMiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStorePicker();
+                mileRadius = 50;
+            }
+        });
+    }
+    public void openStorePicker() {
+        Intent openStore = new Intent(this, StorePicker.class);
+        startActivity(openStore);
     }
 
     //We'll start here tomorrow!
---
+++ app/src/main/java/com/example/cs125finalproject/StorePicker.java
@@ -0,0 +1,54 @@
+package com.example.cs125finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class StorePicker extends AppCompatActivity {
+
+    private Button continueButton;
+    private CheckBox saveALot, aldi, countyMarket;
+    public boolean saveALotChosen = false;
+    public boolean aldiChosen = false;
+    public boolean countyMarketChosen = false;
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store__picker);
+
+
+        continueButton = (Button) findViewById(R.id.continueButton);
+        saveALot = (CheckBox) findViewById(R.id.saveALotCheck);
+        aldi = (CheckBox) findViewById(R.id.aldiCheck);
+        countyMarket = (CheckBox) findViewById(R.id.countyMarketCheck);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!saveALot.isChecked() && !aldi.isChecked() && !countyMarket.isChecked()) {
+                    Toast.makeText(StorePicker.this, "Error: You did not choose a store, please select one.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (saveALot.isChecked()) {
+                    saveALotChosen = true;
+                }
+                if (aldi.isChecked()) {
+                    aldiChosen = true;
+                }
+                if (countyMarket.isChecked()) {
+                    countyMarketChosen = true;
+                }
+                openViewOutput();
+            }
+        });
+    }
+    public void openViewOutput() {
+        Intent openView = new Intent(this, viewOutput.class);
+        startActivity(openView);
+    }
+}
